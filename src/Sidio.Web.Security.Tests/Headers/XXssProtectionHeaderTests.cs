5bfd644c5f0dbf9caa7bafab51bb2462e800faf1
Marthijn van den Heuvel <marthijn@sidio.nl>
2024-08-12 16:05:46 +0200

    ✅ Added missing test

---
+++ src/Sidio.Web.Security.Tests/Headers/XXssProtectionHeaderTests.cs
@@ -0,0 +1,21 @@
+﻿using Sidio.Web.Security.Headers;
+
+namespace Sidio.Web.Security.Tests.Headers;
+
+public sealed class XXssProtectionHeaderTests
+{
+    private readonly Fixture _fixture = new();
+
+    [Fact]
+    public void Constructor_GivenValue_ShouldSetProperties()
+    {
+        // arrange
+        var value = _fixture.Create<string>();
+
+        // act
+        var header = new XXssProtectionHeader(value);
+
+        // assert
+        header.Value.Should().Be(value);
+    }
+}
