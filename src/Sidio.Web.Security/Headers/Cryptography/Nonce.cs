139b54a18a1a9584e4f6658c18e9461c19d76ecb
Marthijn van den Heuvel <marthijn@sidio.nl>
2024-08-16 10:57:27 +0200

    ✨ Nonce to string

--- src/Sidio.Web.Security/Headers/Cryptography/Nonce.cs
+++ src/Sidio.Web.Security/Headers/Cryptography/Nonce.cs
@@ -1,9 +1,19 @@
-﻿using System.Security.Cryptography;
+﻿using System.Diagnostics;
+using System.Security.Cryptography;
 
 namespace Sidio.Web.Security.Headers.Cryptography;
 
+/// <summary>
+/// The Nonce class represents a cryptographic nonce.
+/// </summary>
+[DebuggerDisplay("{Value}")]
 public sealed record Nonce
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Nonce"/> class.
+    /// </summary>
+    /// <param name="value">The value.</param>
+    /// <exception cref="ArgumentException">Thrown when the value is null or empty.</exception>
     public Nonce(string value)
     {
         if (string.IsNullOrWhiteSpace(value))
@@ -14,8 +24,17 @@ public sealed record Nonce
         Value = value;
     }
 
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
     public string Value { get; }
 
+    /// <summary>
+    /// Creates a new nonce with a specified length.
+    /// </summary>
+    /// <param name="length">The length.</param>
+    /// <returns>A <see cref="Nonce"/>.</returns>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when the length is invalid.</exception>
     public static Nonce Create(int length = 32)
     {
         if (length < 1)
@@ -28,4 +47,7 @@ public sealed record Nonce
         rnd.GetBytes(bytes);
         return new Nonce(bytes.Base64UrlEncode());
     }
+
+    /// <inheritdoc />
+    public override string ToString() => Value;
 }
