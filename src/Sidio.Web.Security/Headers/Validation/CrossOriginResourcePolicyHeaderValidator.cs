6fc37d07f76e2bf2fb4b4dd92328a04f92bbc4ca
Marthijn van den Heuvel <marthijn@sidio.nl>
2025-02-15 10:09:02 +0100

    ✨ Implement Cross-Origin Resource Policy header validation and add unit tests

--- src/Sidio.Web.Security/Headers/Validation/CrossOriginResourcePolicyHeaderValidator.cs
+++ src/Sidio.Web.Security/Headers/Validation/CrossOriginResourcePolicyHeaderValidator.cs
@@ -1,4 +1,5 @@
-﻿using Sidio.Web.Security.Headers.Options;
+﻿using Sidio.Web.Security.Common;
+using Sidio.Web.Security.Headers.Options;
 
 namespace Sidio.Web.Security.Headers.Validation;
 
@@ -10,12 +11,30 @@ public sealed class CrossOriginResourcePolicyHeaderValidator : IHeaderValidator<
     /// <inheritdoc />
     public HeaderValidationResult Validate(string? headerValue, out CrossOriginResourcePolicyHeaderOptions? options)
     {
-        throw new NotImplementedException();
+        if (headerValue.IsNullOrWhiteSpace(out var validations))
+        {
+            options = null;
+            return new HeaderValidationResult(validations);
+        }
+
+        var validation = new List<HeaderValidation>();
+        options = new CrossOriginResourcePolicyHeaderOptions();
+
+        if (!headerValue.TryToEnum(out CrossOriginResourcePolicy policy))
+        {
+            validation.Add(new HeaderValidation(HeaderValidationSeverityLevel.Error, "The header value is not a valid Cross-Origin Resource directive."));
+        }
+        else
+        {
+            options.Policy = policy;
+        }
+
+        return new HeaderValidationResult(validation).ClearOptionsWhenInvalid(ref options);
     }
 
     /// <inheritdoc />
     public HeaderValidationResult Validate(CrossOriginResourcePolicyHeaderOptions options)
     {
-        throw new NotImplementedException();
+        return new HeaderValidationResult([]);
     }
 }
