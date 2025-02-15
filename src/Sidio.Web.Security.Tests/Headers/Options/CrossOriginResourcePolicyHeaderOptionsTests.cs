4ed912af354ba0d0b6969771b4045e06288debe4
Marthijn van den Heuvel <marthijn@sidio.nl>
2025-02-15 09:50:29 +0100

    ✅ Add testcases

--- src/Sidio.Web.Security.Tests/Headers/Options/CrossOriginResourcePolicyHeaderOptionsTests.cs
+++ src/Sidio.Web.Security.Tests/Headers/Options/CrossOriginResourcePolicyHeaderOptionsTests.cs
@@ -4,20 +4,23 @@ namespace Sidio.Web.Security.Tests.Headers.Options;
 
 public sealed class CrossOriginResourcePolicyHeaderOptionsTests
 {
-    [Fact]
-    public void ToString_CrossOriginResourcePolicyHeaderOptions_ReturnsString()
+    [Theory]
+    [InlineData(CrossOriginResourcePolicy.SameSite, "same-site")]
+    [InlineData(CrossOriginResourcePolicy.CrossOrigin, "cross-origin")]
+    [InlineData(CrossOriginResourcePolicy.SameOrigin, "same-origin")]
+    public void ToString_CrossOriginResourcePolicyHeaderOptions_ReturnsString(CrossOriginResourcePolicy policy, string expectedValue)
     {
         // arrange
         var options = new CrossOriginResourcePolicyHeaderOptions
         {
-            Policy = CrossOriginResourcePolicy.CrossOrigin,
+            Policy = policy,
         };
 
         // act
         var result = options.ToString();
 
         // assert
-        result.Should().Be("cross-origin");
+        result.Should().Be(expectedValue);
         result.Should().BeEquivalentTo(options.Value);
     }
 }
